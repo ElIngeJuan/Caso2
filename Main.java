7e5206e33fb984bef6ee6a5443eca513ca327639
Juan Rios <jd.riosn1@uniandes.edu.co>
2024-04-01 02:44:33 -0500

    Refactor code and remove unused file

--- Main.java
+++ Main.java
@@ -1,76 +1,122 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Main {
-    // Función para aplicar el filtro
-    public static void aplicarFiltro(int[][] mat1, int[][] mat2, int[][] mat3, int nf, int nc) {
-        // Aplicar el filtro
-        for (int i = 1; i < nf - 1; i++) {
-            for (int j = 1; j < nc - 1; j++) {
-                // Recorrer los vecinos y aplicar el filtro
-                int acum = 0;
-                for (int a = -1; a <= 1; a++) {
-                    for (int b = -1; b <= 1; b++) {
-                        int i2 = i + a;
-                        int j2 = j + b;
-                        
-                        // Verificar límites para evitar desbordamientos
-                        if (i2 >= 0 && i2 < nf && j2 >= 0 && j2 < nc) {
-                            acum += (mat2[a + 1][b + 1] * mat1[i2][j2]);
+
+    public static void generarReferencias(int tamPaginaBytes, int tamMatriz, int NF, int NC){
+         try {
+            PrintWriter archivoSalida = new PrintWriter(new FileWriter("referencias.txt"));
+
+            int NF_NC_Filtro = 3; // Tamaño del filtro siempre será 3x3
+            int tamValorBytes = 4; // Tamaño de un valor en las matrices en bytes
+
+            // Calcular el número total de registros (NR)
+            int NR = 2 * (NF * NC) + 9; // 3 matrices en total (filtro, datos, resultado)
+
+            // Calcular el número de páginas necesarias para cada matriz
+            int paginasFiltro = (int) Math.ceil((double) NF_NC_Filtro * NF_NC_Filtro * tamValorBytes / tamPaginaBytes);
+            int paginasDatos = (int) Math.ceil((double) NF * NC * tamValorBytes / tamPaginaBytes);
+            int paginasResultado = (int) Math.ceil((double) NF * NC * tamValorBytes / tamPaginaBytes);
+
+            // Calcular el número total de páginas utilizadas (NP)
+            int NP = paginasFiltro + paginasDatos + paginasResultado;
+
+            // Escribir metadatos en el archivo de referencias
+            archivoSalida.println("TP=" + tamPaginaBytes);
+            archivoSalida.println("NF=" + NF);
+            archivoSalida.println("NC=" + NC);
+            archivoSalida.println("NF_NC_Filtro=" + NF_NC_Filtro);
+            archivoSalida.println("NR=" + NR);
+            archivoSalida.println("NP=" + NP);
+
+            for (int i = 1; i < NF - 1; i++) {
+                for (int j = 1; j < NC - 1; j++) {
+                    // Recorrer los vecinos y aplicar el filtro
+                    for (int a = -1; a <= 1; a++) {
+                        for (int b = -1; b <= 1; b++) {
+                            int i2 = i + a;
+                            int j2 = j + b;
+                            int i3 = a + 1;
+                            int j3 = b + 1;
+    
+                           
+                            // Verificar límites para evitar desbordamientos
+
+                            int posInicialM = NF_NC_Filtro * NF_NC_Filtro * tamValorBytes + ((i2) * NC + (j2)) * tamValorBytes;
+                            int paginaM = posInicialM / tamPaginaBytes;
+                            int desplazamientoM = posInicialM % tamPaginaBytes;
+                            archivoSalida.println(String.format("M[%d][%d],%d,%d,R", i2, j2, paginaM, desplazamientoM));
+    
+                            int posInicialF = ((i3) % NF_NC_Filtro) * NF_NC_Filtro * tamValorBytes + ((j3) % NF_NC_Filtro) * tamValorBytes;
+                            int paginaF = posInicialF / tamPaginaBytes;
+                            int desplazamientoF = posInicialF % tamPaginaBytes;
+                            archivoSalida.println(String.format("F[%d][%d],%d,%d,R", i3 % NF_NC_Filtro, j3 % NF_NC_Filtro, paginaF, desplazamientoF));
                         }
                     }
+                    // Aplicar reglas para valores de acumulación
+                    if (i > 0 && i < NF && j > 0 && j < NC) {
+                        System.out.println("R1: " + i + " R2: " + j );
+                        int posInicialR = NF_NC_Filtro * NF_NC_Filtro * tamValorBytes + NF * NC * tamValorBytes + ((i)* NC +(j)) * tamValorBytes;
+                        int paginaR = posInicialR / tamPaginaBytes;
+                        int desplazamientoR = posInicialR % tamPaginaBytes;
+                        archivoSalida.println(String.format("R[%d][%d],%d,%d,W", i, j, paginaR, desplazamientoR));
+                    }
                 }
-                // Aplicar reglas para valores de acumulación
-                if (acum >= 0 && acum <= 255)
-                    mat3[i][j] = acum;
-                else if (acum < 0)
-                    mat3[i][j] = 0;
-                else
-                    mat3[i][j] = 255;
             }
+    
+            // Asignar un valor predefinido a los bordes
+            for (int i = 0; i < NC; i++) {
+                System.out.println("R1: 0 R2: " + i);
+                int posInicialR = NF_NC_Filtro * NF_NC_Filtro * tamValorBytes + NF * NC * tamValorBytes + (0 * NC + i) * tamValorBytes;
+                int paginaR = posInicialR / tamPaginaBytes;
+                int desplazamientoR = posInicialR % tamPaginaBytes;
+                archivoSalida.println(String.format("R[%d][%d],%d,%d,W", 0, i, paginaR, desplazamientoR));
+
+                System.out.println("R1: " + (NF - 1) + " R2: " + i);
+                int posInicialR1 = NF_NC_Filtro * NF_NC_Filtro * tamValorBytes + NF * NC * tamValorBytes + (i * NC + NF - 1) * tamValorBytes;
+                int paginaR1 = posInicialR1 / tamPaginaBytes;
+                int desplazamientoR1 = posInicialR1 % tamPaginaBytes;
+                archivoSalida.println(String.format("R[%d][%d],%d,%d,W", NF - 1, i, paginaR1, desplazamientoR1));
+
+            }
+            for (int i = 1; i < NF - 1; i++) {
+                System.out.println("R1: " + i + " R2: 0");
+                int posInicialR = NF_NC_Filtro * NF_NC_Filtro * tamValorBytes + NF * NC * tamValorBytes + (i * NC + 0) * tamValorBytes;
+                int paginaR = posInicialR / tamPaginaBytes;
+                int desplazamientoR = posInicialR % tamPaginaBytes;
+                archivoSalida.println(String.format("R[%d][%d],%d,%d,W", i, 0, paginaR, desplazamientoR));
+
+                System.out.println("R1: " + i + " R2: " + (NC - 1));
+                int posInicialR1 = NF_NC_Filtro * NF_NC_Filtro * tamValorBytes + NF * NC * tamValorBytes + (i * NC + NC - 1) * tamValorBytes;
+                int paginaR1 = posInicialR1 / tamPaginaBytes;
+                int desplazamientoR1 = posInicialR1 % tamPaginaBytes;
+                archivoSalida.println(String.format("R[%d][%d],%d,%d,W", i, NC - 1, paginaR1, desplazamientoR1));
+
+            }
+
+            archivoSalida.close();
+            System.out.println("Archivo de referencias generado: referencias.txt");
+        } catch (IOException e) {
+            System.out.println("Error al generar el archivo de referencias.");
         }
 
-        // Asignar un valor predefinido a los bordes
-        for (int i = 0; i < nc; i++) {
-            mat3[0][i] = 0;
-            mat3[nf - 1][i] = 255;
-        }
-        for (int i = 1; i < nf - 1; i++) {
-            mat3[i][0] = 0;
-            mat3[i][nc - 1] = 255;
-        }
     }
 
     public static void main(String[] args) {
-        // Matriz de datos de ejemplo (5x5)
-        int[][] datos = {
-            {1, 2, 3, 4},
-            {5, 6, 7, 8},
-            {9, 10, 11, 12},
-            {13, 14, 15, 16}
-        };
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Ingrese el tamaño de página (en bytes): ");
+            int tamPaginaBytes = scanner.nextInt();
+            System.out.print("Ingrese el número de filas de la matriz de datos: ");
+            int NF = scanner.nextInt();
+            System.out.print("Ingrese el número de columnas de la matriz de datos: ");
+            int NC = scanner.nextInt();
 
-        // Matriz de filtro de ejemplo (3x3)
-        int[][] filtro = {
-            {1, 0, -1},
-            {1, 0, -1},
-            {1, 0, -1}
-        };
+            int tamMatriz = Math.max(NF, NC); // Tamaño de la matriz de datos (máximo entre NF y NC)
 
-        // Tamaño de filas y columnas de las matrices de datos de ejemplo
-        int filas = datos.length;
-        int columnas = datos[0].length;
-
-        // Matriz resultante
-        int[][] resultado = new int[filas][columnas];
-
-        // Aplicar el filtro a los datos de ejemplo
-        aplicarFiltro(datos, filtro, resultado, filas, columnas);
-
-        // Imprimir la matriz resultante
-        System.out.println("Matriz resultante después de aplicar el filtro:");
-        for (int i = 0; i < filas; i++) {
-            for (int j = 0; j < columnas; j++) {
-                System.out.print(resultado[i][j] + "\t");
-            }
-            System.out.println();
+            generarReferencias(tamPaginaBytes, tamMatriz, NF, NC);
         }
     }
+
 }
